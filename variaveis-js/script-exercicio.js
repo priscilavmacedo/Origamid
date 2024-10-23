751b4da6b4307b88698f306efd0db1a13ab6ee25
priscila.macedo <priscila.macedo@saude.ba.gov.br>
2024-10-23 17:23:22 -0300

    upt: mudanca de nome

---
+++ variaveis-js/script-exercicio.js
@@ -0,0 +1,33 @@
+
+
+//declarar variavel com nome
+//declarar variavel com idade
+//declarar variavel com comida
+
+ var n = 'priscila';
+
+ var idade = 35;
+
+ console.log(n,idade);
+ 
+
+// declarar uma variavel com a sua comida favorirta e nao atribuir valor
+ var comidaFavorita;
+
+//atribuir valor a comida favorita
+ var comidaFavorita = 'feijoada'
+ console.log(comidaFavorita);
+ 
+
+
+ //declarando variaveis sem valores, pode ser das duas formas
+var semDefinirUm
+var semDefinirDois
+var semDefinirTres
+var semDefinirQuatro
+
+var semDefinirUm, semDefinirDois, semDefinirTres,semDefinirQuatro
+ 
+
+
+ 
---
+++ variaveis-js/script.js
@@ -0,0 +1,50 @@
+
+var nome ='andre'
+var idade = 23
+var possuiFaculdade = true
+
+console.log(nome,idade, possuiFaculdade);
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado *preco 
+
+console.log(totalPreco);
+
+var sobrenome = 'rafeal' , cidade = 'rio';
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaodefini);
+//aqui ele debuga o codigo e da a linha do erro no inspecionar
+// quando dá o erro ele para de funcioanr
+
+
+// const time = 'vasco';
+// time = 'flamengo';
+
+// console.log(time);
+ //cont nao muda , pois é algo que declarei e é unico.
+
+ var n = 'priscila';
+
+ var idade = 35;
+
+ var comida = 'feijoada'
+
+ var valor = 15
+
+
+ console.log(n,idade,comida);
+
+ //declarando variaveis sem valores
+var semDefinirUm
+var semDefinirDois
+var semDefinirTres
+var semDefinirQuatro
+ 
+
+
+ 
